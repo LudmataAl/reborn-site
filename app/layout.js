b8d2b14074c15b8b435ddbdec833883273511e44
LudmataAl <alexsunoson@gmail.com>
2026-03-30 00:53:51 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'REBORN',
+  description: 'Rebuild the System'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
